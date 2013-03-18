fba5f880e27fad1d567bc69cfca01507822f2f95
hex <hex@hex.cat>
2013-03-18 15:36:26 +0100

    Update Crawl.php
    Just removed your old password from the code example.

--- src/Ingress/Crawl.php
+++ src/Ingress/Crawl.php
@@ -17,7 +17,7 @@ use Guzzle\Plugin\Backoff\BackoffPlugin;
 class Crawl
 {
     const USER = 'florinutz';
-    const PASSWORD = 'iataNumarul7';
+    const PASSWORD = 'myPassword';
     const URL_START = 'http://www.ingress.com/intel';
     const URL_LOGIN = 'https://accounts.google.com/ServiceLogin?service=ah&passive=true&continue=https://appengine.google.com/_ah/conflogin%3Fcontinue%3Dhttp://www.ingress.com/intel';
     const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11';
